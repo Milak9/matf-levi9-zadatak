f605199c64bd2cba17eca28a2a0b32d2a1871e27
Milos Milakovic <milos.milakovic98@gmail.com>
2020-12-19 23:01:25 +0100

    Dodat BrowseRouter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="admin">Admin</Link>
+          </li>
+        </ul>
+      </nav>
+
+    <Switch>
+      <Route exact path="/"><Home/></Route>
+      <Route path="/admin"><Admin/></Route>
+    </Switch>
+
     </div>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Admin() {
+  return <h2>Admin</h2>;
+}
+
+function Nesto() {
+  return <h2>Users</h2>;
+}

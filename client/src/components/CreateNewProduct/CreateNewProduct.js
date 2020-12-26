622840d4e0cd9730ed15ea3dd8b48382f7f3eeff
Milos Milakovic <milos.milakovic98@gmail.com>
2020-12-26 12:09:35 +0100

    Dodata provera
    Dodata je provera da li su zadate ispravne vrednosti prilikom unosenja
    proizvoda u bazu. Ne moze da se unese proizvod kome nije zadato ime
    kao ni proizvod cija je cena manje ili jednaka od nule.

--- client/src/components/CreateNewProduct/CreateNewProduct.js
+++ client/src/components/CreateNewProduct/CreateNewProduct.js
@@ -8,56 +8,73 @@ class CreateNewProduct extends React.Component {
     state = {
         name: "",
         description: "",
-        price: 0
+        price: 0,
+        mistake: false
     };
 
     render() {
-        return (
-            <div className="new-product">
-                <h2> Add new product</h2>
-                <form onSubmit={this.onFormSubmit}>
-                    <div>
-                        <label>Name:</label>
-                        <input
-                            name="name"
-                            type="text"
-                            onChange={this.handleChange}
-                            value={this.state.name}
-                            placeholder="Name of the product"
-                        />
-                    </div>
-                    <div>
-                        <label>Description:</label>
-                        <textarea
-                            name="description"
-                            onChange={this.handleChange}
-                            value={this.state.description}
-                            placeholder="Description of the product"
-                        />
-                    </div>
-                    <div>
-                        <label>Price:</label>
-                        <input
-                            name="price"
-                            type="number"
-                            onChange={this.handleChange}
-                            value={this.state.price}
-                            placeholder="Price of the product"
-                        />
-                    </div>
-                    <button className="btn_create" type="submit">
-                        Create
-                    </button>
-                </form>
-            </div>
-        );
+        return this.state.mistake ? this.renderMistake() : this.renderForm();
+
     }
+
+    renderForm = () => (
+        <div className="new-product">
+            <h2> Add new product</h2>
+            <form onSubmit={this.onFormSubmit}>
+                <div>
+                    <label>Name:</label>
+                    <input
+                        name="name"
+                        type="text"
+                        onChange={this.handleChange}
+                        value={this.state.name}
+                        placeholder="Name of the product"
+                    />
+                </div>
+                <div>
+                    <label>Description:</label>
+                    <textarea
+                        name="description"
+                        onChange={this.handleChange}
+                        value={this.state.description}
+                        placeholder="Description of the product"
+                    />
+                </div>
+                <div>
+                    <label>Price:</label>
+                    <input
+                        name="price"
+                        type="number"
+                        onChange={this.handleChange}
+                        value={this.state.price}
+                        placeholder="Price of the product"
+                    />
+                </div>
+                <button className="btn_create" type="submit">
+                    Create
+                </button>
+            </form>
+        </div> 
+    );
+
+    renderMistake = () => 
+        <div>
+            Error, you can't leave empty name or set negative or 0 value for the price of the product. 
+        </div>
 
     onFormSubmit = event => {
         event.preventDefault();
-        console.log(this.state);
 
-        const { name, description, price } = this.state;
+        let { name, description, price, mistake } = this.state;
+
+        if (name === "" || price <= 0) {
+            this.setState({
+                mistake: true
+            });
+            return;
+        }
+
+        console.log(this.state);
 
         productsApi.createProduct({ name, description, price})
             .then(() => {
